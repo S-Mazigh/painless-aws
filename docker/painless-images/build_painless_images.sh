6449f5eab242f1652cda5615c8edb7d04574def1
Saoudi Mazigh <mazigh.c@outlook.com>
2024-05-01 02:27:57 +0200

    docker images works with dist

--- docker/painless-images/build_painless_images.sh
+++ docker/painless-images/build_painless_images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --rm -t satcomp-painless:common ../../ --file common/Dockerfile
-docker build --rm -t satcomp-painless:leader ../../ --file leader/Dockerfile
-docker build --rm -t satcomp-painless:worker ../../ --file worker/Dockerfile
+docker build -t satcomp-painless:common --file common/Dockerfile .
+docker build -t satcomp-painless:leader  --file leader/Dockerfile .
+docker build -t satcomp-painless:worker  --file worker/Dockerfile .
--- docker/painless-images/nocache_painless_images.sh
+++ docker/painless-images/nocache_painless_images.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build --rm --no-cache -t satcomp-painless:common ../../ --file common/Dockerfile
-docker build --rm --no-cache -t satcomp-painless:leader ../../ --file leader/Dockerfile
-docker build --rm --no-cache -t satcomp-painless:worker ../../ --file worker/Dockerfile
+docker build --no-cache -t satcomp-painless:common --file common/Dockerfile .
+docker build --no-cache -t satcomp-painless:leader --file leader/Dockerfile .
+docker build --no-cache -t satcomp-painless:worker --file worker/Dockerfile .
